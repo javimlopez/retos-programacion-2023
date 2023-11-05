35d26949b11e22d92c58804450d93db5299ae6f3
Francisco Javier Mota López <fml200@inlumine.ual.es>
2023-11-05 20:56:33 +0100

    Reto #0 - Haskell

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/haskell/javimlopez.hs
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/haskell/javimlopez.hs
@@ -4,6 +4,6 @@ fizzbuzz x
     | mod x 3 == 0 = "fizz"
     | mod x 5 == 0 = "buzz"
     | (mod x 3 == 0) && (mod x 5 == 0) = "fizzbuzz"
-    | otherwise = show x 
+    | otherwise = show x
 
 imprimir = mapM_ putStrLn [fizzbuzz x | x <- [1..100]]
